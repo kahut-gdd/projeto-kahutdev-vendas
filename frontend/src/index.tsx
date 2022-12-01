a725e0008bdc2575fbcbd1337940eb5b9a519945
kahut. dev <dagmar.kahutpp@gmail.com>
2022-12-01 13:51:39 -0300

    Basic static components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
